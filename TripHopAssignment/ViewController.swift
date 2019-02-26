06841c7168ef146344cf04c519f12ef1bbb71f1f
Ahmed Qazzaz <ahmedqazzaz@iahmed.local>
2019-02-26 05:57:31 +0200

    Initial Commit

---
+++ TripHopAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripHopAssignment
+//
+//  Created by Ahmed Qazzaz on 2/26/19.
+//  Copyright © 2019 Ahmed Qazzaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
